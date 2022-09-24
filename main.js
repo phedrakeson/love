700d01e0808097fa4b8c9a79225bc5b47e3c28f2
Gabriel Duarte <gabrield.developer@gmail.com>
2022-09-24 00:27:40 -0300

    feat: creates runner function on mouseover to a specific button

---
+++ main.js
@@ -0,0 +1,29 @@
+const runner = document.getElementById('runner');
+const wrapper = document.querySelector('.wrapper');
+
+runner.addEventListener('mouseover', () => {
+  const wrapperHeight = wrapper.offsetHeight;
+  const wrapperWidth = wrapper.offsetWidth;
+  let offsetX = runner.offsetLeft;
+  let offsetY = runner.offsetTop;
+  let goX = Math.random() < 0.5 ? -1 : 1;
+  let goY = Math.random() < 0.5 ? -1 : 1;
+  let targetSize = runner.offsetWidth;
+
+  if (
+    goX > 0 && offsetX + targetSize * goX + targetSize > wrapperHeight ||
+    goX < 0 && offsetX + targetSize * goX - targetSize < 0
+  ) {
+    goX = goX * (-1);
+  }
+
+  if (
+    goY > 0 && offsetY + targetSize * goY + targetSize > wrapperWidth ||
+    goY < 0 && offsetY + targetSize * goY - targetSize < 0
+  ) {
+    goY = goY * (-1);
+  }
+
+  runner.style.top = offsetY + 150 * goY + 'px';
+  runner.style.left = offsetX + 150 * goX + 'px';
+})
