735f2b023024d299de3ffc6ee0564ec571ee4efc
antonioal97 <antonioal97@gmail.com>
2022-10-12 18:12:44 -0300

    Logica basica (ainda necessario algumas modificacoes)

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+import counting from './counter.js'
+
+const reject = document.querySelector('.button--reject')
+const accept = document.querySelector('.button--accept')
+
+const proximit = () => {
+  // Passar como argumento o botão
+
+  const positions = {
+    top: reject.offsetTop,
+    left: reject.offsetLeft,
+    randomY() {
+      return Math.random() * this.top
+    },
+    randomX() {
+      return Math.random() * this.left
+    }
+  }
+
+  const moveToRight = reject.addEventListener('mouseenter', () => {
+    let count = 0
+
+    reject.style.transform += `translateX(${positions.randomX()}px)`
+
+    let moveToDown = setInterval(() => {
+      count += 1
+
+      reject.style.transform += `translateY(${positions.randomY()}px)`
+
+      if (count === 3) {
+        clearInterval(moveToDown)
+        reject.style.removeProperty('transform')
+      }
+    }, 650)
+  })
+
+}
+
+proximit()
+
+counting(accept)
