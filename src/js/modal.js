a9d44719582e77cf648a522279a58edfa36e21ea
antonioal97 <antonioal97@gmail.com>
2022-10-14 19:12:10 -0300

    Logica basica do modal (ainda necessario algumas modificacoes)

---
+++ src/js/modal.js
@@ -0,0 +1,37 @@
+export const modal = document.querySelector('#modal')
+
+const img = {
+  el: modal.querySelector('img'),
+  positive: '../img/icon/after-10-yes.svg',
+  negative: '../img/icon/after-10-not-tentatives.svg'
+}
+
+const text = {
+  el: modal.querySelector('p'),
+  positive:
+    'Te entrego meu coração para que dele cuides tão bem quanto cuidarei do seu.',
+  negative:
+    '  Não se acanhe, nosso amor é azul como o mar azul, nada pode mudar isso I Love You S2'
+}
+
+const modalPositive = {
+  img: img.positive,
+  text: text.positive
+}
+
+const modalNegative = {
+  img: img.negative,
+  text: text.negative
+}
+
+modal.querySelector('.button--accept').addEventListener('click', () => {
+  modal.close()
+  modal.classList.remove('is-inactive')
+})
+
+export function modalPos() {
+  modal.showModal()
+  img.el.setAttribute('src', img.positive)
+  text.el.setAttribute('src', text.positive)
+
+}
